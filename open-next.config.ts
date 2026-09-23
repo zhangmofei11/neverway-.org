64f854720daa9bb199f505a3ea597cf64555c1b2
zhangmofei11 <2312510836@qq.com>
2026-09-23 14:11:25 +0800

    Fix open-next.config.ts for Cloudflare

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,24 @@
-import type { OpenNextConfig } from '@opennextjs/cloudflare'
-
-const config: OpenNextConfig = {
+const config = {
   default: {
     override: {
-      wrapper: 'cloudflare-edge',
-      converter: 'edge',
-      incrementalCache: 'cloudflare-kv',
-      tagCache: 'cloudflare-kv',
-      queue: 'cloudflare-queue',
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: [],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 }
